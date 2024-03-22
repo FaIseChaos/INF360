5f0f53dc646313e3e90282d4aa03b16d11219c44
Jordin Myers <jordinmyers@gmail.com>
2024-03-21 20:07:32 -0500

    worked on sudoku solver

--- Projects/SudokuVersion2.py
+++ Projects/SudokuVersion2.py
@@ -8,6 +8,7 @@ from time import sleep as sleep
 from pynput import keyboard # pip install pynput
 from pynput.keyboard import Key
 import os
+import fontstyle # pip install fontstyle
 
 #set up board data
 r = {
@@ -15,52 +16,10 @@ r = {
     for i in range(9)
 }
 
-""""r = { old dictionary
-    0: ["X",".",".",".",".",".",".",".","."],
-    1: [".",".",".",".",".",".",".",".","."],
-    2: [".",".",".",".",".",".",".",".","."],
-    3: [".",".",".",".",".",".",".",".","."],
-    4: [".",".",".",".",".",".",".",".","."],
-    5: [".",".",".",".",".",".",".",".","."],
-    6: [".",".",".",".",".",".",".",".","."],
-    7: [".",".",".",".",".",".",".",".","."],
-    8: [".",".",".",".",".",".",".",".","."]
-}"""
-
 c = { # zip function, will do one "round" of the the "j" for loop first, then one "round" of the "i" for loop, then back to "j", then "i", etc.
     i: [r[j][i] for j in range(9)] 
     for i in range(9)
 }
-
-"""c = { old dictionary
-    0: [r[0][0],r[1][0],r[2][0],r[3][0],r[4][0],r[5][0],r[6][0],r[7][0],r[8][0]],
-    1: [r[0][1],r[1][1],r[2][1],r[3][1],r[4][1],r[5][1],r[6][1],r[7][1],r[8][1]],
-    2: [r[0][2],r[1][2],r[2][2],r[3][2],r[4][2],r[5][2],r[6][2],r[7][2],r[8][2]],
-    3: [r[0][3],r[1][3],r[2][3],r[3][3],r[4][3],r[5][3],r[6][3],r[7][3],r[8][3]],
-    4: [r[0][4],r[1][4],r[2][4],r[3][4],r[4][4],r[5][4],r[6][4],r[7][4],r[8][4]],
-    5: [r[0][5],r[1][5],r[2][5],r[3][5],r[4][5],r[5][5],r[6][5],r[7][5],r[8][5]],
-    6: [r[0][6],r[1][6],r[2][6],r[3][6],r[4][6],r[5][6],r[6][6],r[7][6],r[8][6]],
-    7: [r[0][7],r[1][7],r[2][7],r[3][7],r[4][7],r[5][7],r[6][7],r[7][7],r[8][7]],
-    8: [r[0][8],r[1][8],r[2][8],r[3][8],r[4][8],r[5][8],r[6][8],r[7][8],r[8][8]]
-}"""
-
-b = {
-    0: [r[0][0],r[0][1],r[0][2],r[1][0],r[1][1],r[1][2],r[2][0],r[2][1],r[2][2]],
-    1: [r[0][3],r[0][4],r[0][5],r[1][3],r[1][4],r[1][5],r[2][3],r[2][4],r[2][5]],
-    2: [r[0][6],r[0][7],r[0][8],r[1][6],r[1][7],r[1][8],r[2][6],r[2][7],r[2][8]],
-    3: [r[3][0],r[3][1],r[3][2],r[4][0],r[4][1],r[4][2],r[5][0],r[5][1],r[5][2]],
-    4: [r[3][3],r[3][4],r[3][5],r[4][3],r[4][4],r[4][5],r[5][3],r[5][4],r[5][5]],
-    5: [r[3][6],r[3][7],r[3][8],r[4][6],r[4][7],r[4][8],r[5][6],r[5][7],r[5][8]],
-    6: [r[6][0],r[6][1],r[6][2],r[7][0],r[7][1],r[7][2],r[8][0],r[8][1],r[8][2]],
-    7: [r[6][3],r[6][4],r[6][5],r[7][3],r[7][4],r[7][5],r[8][3],r[8][4],r[8][5]],
-    8: [r[6][6],r[6][7],r[6][8],r[7][6],r[7][7],r[7][8],r[8][6],r[8][7],r[8][8]]
-}
-
-"""
-b = { new dictionary I do not understand
-    i: [r[row][col] for row in range(i//3*3, i//3*3+3) for col in range(i%3*3, i%3*3+3)] for i in range(9)
-}
-"""
 
 n = ['1','2','3','4','5','6','7','8','9']
 
@@ -93,12 +52,8 @@ def board():
 
 def clean(row,col):
     os.system('cls')
-    if r[row][col] == "X":
-        r[row][col] = "."    
-    #return
-
-def loc(row,col):
-    r[row][col] = "X"
+    #r[row][col] = fontstyle.apply(str(r[pos[0]][pos[1]]),'WHITE/BLACK_BG')
+    fontstyle.preserve(r[pos[0]][pos[1]])
 
 def right():
     clean(pos[0],pos[1])
@@ -107,7 +62,8 @@ def right():
     elif pos[1] == 8 and pos[0] < 8:
         pos[1] = 0
         pos[0] += 1
-    loc(pos[0],pos[1])
+    font = fontstyle.apply(str(r[pos[0]][pos[1]]),'BLACK/WHITE_BG')
+    r[pos[0]][pos[1]] = font
     print(message,board())
 
 def left():
@@ -117,56 +73,64 @@ def left():
     elif pos[1] == 0 and pos[0] > 0:
         pos[1] = 8
         pos[0] -= 1
-    loc(pos[0],pos[1])
+    r[pos[0]][pos[1]] = fontstyle.apply(str(r[pos[0]][pos[1]]),'BLACK/WHITE_BG')
     print(message,board())
 
 def up():
     clean(pos[0],pos[1])
     if pos[0] > 0:
         pos[0] -= 1
-    loc(pos[0],pos[1])
+    r[pos[0]][pos[1]] = fontstyle.apply(str(r[pos[0]][pos[1]]),'BLACK/WHITE_BG')
     print(message,board())
 
 def down():
     clean(pos[0],pos[1])
     if pos[0] < 8:
         pos[0] += 1
-    loc(pos[0],pos[1])
+    r[pos[0]][pos[1]] = fontstyle.apply(str(r[pos[0]][pos[1]]),'BLACK/WHITE_BG')
     print(message,board())
 
-def check(row,digit,f):
-    num = [1,2,3,4,5,6,7,8,9]
-    if f == 'row':
-        for i in r[row]:
-            if i in num:
-                num.remove(i)
-    elif f == 'col':
-        for i in c[row]:
-            if i in num:
-                num.remove(i)
-    elif f == 'box':
-        for i in b[row]:
-            if i in num:
-                num.remove(i)
-    if digit in num:
-        return True
-    else:
-        return False
-
 def solve():
-    for i in range(9):
-        for j in range(9):
-            if r[i][j] == ".":
-                for n in range(1,10):
-                    if check(i,n,'row') and check(i,n,'col') and check(i,n,'box'):
-                        r[i][j] = n
-                        solve()
-                        r[i][j] = "."
-                return
     return
 
+
+
+def solve(row, col, num):
+    for x in range(9):
+        if r[row][x] == num:
+            return False
+    for x in range(9):
+        if r[x][col] == num:
+            return False
+    startRow = row - row % 3
+    startCol = col - col % 3
+    for i in range(3):
+        for j in range(3):
+            if r[i + startRow][j + startCol] == num:
+                return False
+    return True
+ 
+def Suduko(row, col):
+    if (row == 8 and col == 9):
+        return True
+    if col == 9:
+        row += 1
+        col = 0
+    if r[row][col] != ".":
+        return Suduko(row, col + 1)
+    for num in range(1, 10): 
+        if solve(row, col, num):
+            r[row][col] = num
+            if Suduko(row, col + 1):
+                return True
+        r[row][col] = "."
+    return False
+
+
+
+
+
 #main loop
-r[0][0] = 'X'
 print(message,board())
 def on_key_release(key):
     if key == Key.right:
@@ -178,17 +142,23 @@ def on_key_release(key):
     elif key == Key.down:
         down()
     elif key == Key.esc:
+        print("Exiting...")
+        sleep(1)
         exit()
     elif key == Key.space:
         print(r)
+    elif key == Key.enter:
+        print("Solving...")
+        Suduko(0,0)
+        #solve()
     elif key.char in n:
         num = int(key.char)
         r[pos[0]][pos[1]] = num
         right()
         #return
-    elif key == Key.enter:
-        print("Solving...")
-
-
+    else:
+        print("Invalid input1")
+        return
+    
 with keyboard.Listener(on_release=on_key_release) as listener:
     listener.join()
