d6f3530dbe0bb427c4d3b6b11b63f25d80b99269
Jordin Myers <jordinmyers@gmail.com>
2024-03-22 16:21:59 -0500

    tidied up variables, functions, and removed repeated code

--- Projects/SudokuVersion2.py
+++ Projects/SudokuVersion2.py
@@ -8,12 +8,11 @@ from datetime import datetime
 import os
 
 #set up board
-r = { # creates a 9x9 board with all values set to "." - backend use
+boardData = { # creates a 9x9 board with all values set to "." - backend use
     i: ["."]*9 for i in range(9)
 }
-n = ['1','2','3','4','5','6','7','8','9'] # used to check if the input is a number
 pos = [0,0] # starting position
-s = False # used to check if the board is solved
+solved = False # used to check if the board is solved
 message = """ 
 Use the arrow keys to move the cursor and the number keys to enter a number.
 Press esc to exit.
@@ -21,122 +20,122 @@ Press enter to submit.
 """ # instructions
 
 #functions
-def board(): # creates the board that will be displayed to user
+def create_board(): # creates the board that will be displayed to user
     b = f"""
         -------------------------
-        | {r[0][0]} {r[0][1]} {r[0][2]} | {r[0][3]} {r[0][4]} {r[0][5]} | {r[0][6]} {r[0][7]} {r[0][8]} |
-        | {r[1][0]} {r[1][1]} {r[1][2]} | {r[1][3]} {r[1][4]} {r[1][5]} | {r[1][6]} {r[1][7]} {r[1][8]} |
-        | {r[2][0]} {r[2][1]} {r[2][2]} | {r[2][3]} {r[2][4]} {r[2][5]} | {r[2][6]} {r[2][7]} {r[2][8]} |
+        | {boardData[0][0]} {boardData[0][1]} {boardData[0][2]} | {boardData[0][3]} {boardData[0][4]} {boardData[0][5]} | {boardData[0][6]} {boardData[0][7]} {boardData[0][8]} |
+        | {boardData[1][0]} {boardData[1][1]} {boardData[1][2]} | {boardData[1][3]} {boardData[1][4]} {boardData[1][5]} | {boardData[1][6]} {boardData[1][7]} {boardData[1][8]} |
+        | {boardData[2][0]} {boardData[2][1]} {boardData[2][2]} | {boardData[2][3]} {boardData[2][4]} {boardData[2][5]} | {boardData[2][6]} {boardData[2][7]} {boardData[2][8]} |
         -------------------------
-        | {r[3][0]} {r[3][1]} {r[3][2]} | {r[3][3]} {r[3][4]} {r[3][5]} | {r[3][6]} {r[3][7]} {r[3][8]} |
-        | {r[4][0]} {r[4][1]} {r[4][2]} | {r[4][3]} {r[4][4]} {r[4][5]} | {r[4][6]} {r[4][7]} {r[4][8]} |
-        | {r[5][0]} {r[5][1]} {r[5][2]} | {r[5][3]} {r[5][4]} {r[5][5]} | {r[5][6]} {r[5][7]} {r[5][8]} |
+        | {boardData[3][0]} {boardData[3][1]} {boardData[3][2]} | {boardData[3][3]} {boardData[3][4]} {boardData[3][5]} | {boardData[3][6]} {boardData[3][7]} {boardData[3][8]} |
+        | {boardData[4][0]} {boardData[4][1]} {boardData[4][2]} | {boardData[4][3]} {boardData[4][4]} {boardData[4][5]} | {boardData[4][6]} {boardData[4][7]} {boardData[4][8]} |
+        | {boardData[5][0]} {boardData[5][1]} {boardData[5][2]} | {boardData[5][3]} {boardData[5][4]} {boardData[5][5]} | {boardData[5][6]} {boardData[5][7]} {boardData[5][8]} |
         -------------------------
-        | {r[6][0]} {r[6][1]} {r[6][2]} | {r[6][3]} {r[6][4]} {r[6][5]} | {r[6][6]} {r[6][7]} {r[6][8]} |
-        | {r[7][0]} {r[7][1]} {r[7][2]} | {r[7][3]} {r[7][4]} {r[7][5]} | {r[7][6]} {r[7][7]} {r[7][8]} |
-        | {r[8][0]} {r[8][1]} {r[8][2]} | {r[8][3]} {r[8][4]} {r[8][5]} | {r[8][6]} {r[8][7]} {r[8][8]} |
+        | {boardData[6][0]} {boardData[6][1]} {boardData[6][2]} | {boardData[6][3]} {boardData[6][4]} {boardData[6][5]} | {boardData[6][6]} {boardData[6][7]} {boardData[6][8]} |
+        | {boardData[7][0]} {boardData[7][1]} {boardData[7][2]} | {boardData[7][3]} {boardData[7][4]} {boardData[7][5]} | {boardData[7][6]} {boardData[7][7]} {boardData[7][8]} |
+        | {boardData[8][0]} {boardData[8][1]} {boardData[8][2]} | {boardData[8][3]} {boardData[8][4]} {boardData[8][5]} | {boardData[8][6]} {boardData[8][7]} {boardData[8][8]} |
         -------------------------
         """
     return b
 
-def clean(row,col): # used to clean the terminal
+def clean_terminal(row,col): # used to clean the terminal
     os.system('cls')
-    if r[row][col] == "X":
-        r[row][col] = "."
+    if boardData[row][col] == "X":
+        boardData[row][col] = "."
 
-def right(): # moves the cursor to the right
-    clean(pos[0],pos[1])
+def move_right(): # moves the cursor to the right
+    clean_terminal(pos[0],pos[1])
     if pos[1] < 8:
         pos[1] += 1
     elif pos[1] == 8 and pos[0] < 8:
         pos[1] = 0
         pos[0] += 1
-    r[pos[0]][pos[1]] = "X"
-    print(message,board())
+    boardData[pos[0]][pos[1]] = "X"
+    print(message,create_board())
 
-def left(): # moves the cursor to the left
-    clean(pos[0],pos[1])
+def move_left(): # moves the cursor to the left
+    clean_terminal(pos[0],pos[1])
     if pos[1] > 0:
         pos[1] -= 1
     elif pos[1] == 0 and pos[0] > 0:
         pos[1] = 8
         pos[0] -= 1
-    r[pos[0]][pos[1]] = "X"
-    print(message,board())
+    boardData[pos[0]][pos[1]] = "X"
+    print(message,create_board())
 
-def up(): # moves the cursor up
-    clean(pos[0],pos[1])
+def move_up(): # moves the cursor up
+    clean_terminal(pos[0],pos[1])
     if pos[0] > 0:
         pos[0] -= 1
-    r[pos[0]][pos[1]] = "X"
-    print(message,board())
+    boardData[pos[0]][pos[1]] = "X"
+    print(message,create_board())
 
-def down(): # moves the cursor down
-    clean(pos[0],pos[1])
+def move_down(): # moves the cursor down
+    clean_terminal(pos[0],pos[1])
     if pos[0] < 8:
         pos[0] += 1
-    r[pos[0]][pos[1]] = "X"
-    print(message,board())
+    boardData[pos[0]][pos[1]] = "X"
+    print(message,create_board())
 
-def check(row, col, num): # logic to check if the number is valid
+def check_if_valid(row, col, num): # logic to check if the number is valid
     for i in range(9): #checking the row
-        if r[row][i] == num:
+        if boardData[row][i] == num:
             return False
     for i in range(9): #checking the column
-        if r[i][col] == num:
+        if boardData[i][col] == num:
             return False
     startRow = row - row % 3 #used to check the cells
     startCol = col - col % 3
     for i in range(3): #checking the cells
         for j in range(3):
-            if r[i + startRow][j + startCol] == num:
+            if boardData[i + startRow][j + startCol] == num:
                 return False
     return True 
  
-def solve(row, col):
+def solve_puzzle(row, col):
     startTime = datetime.now()
     if (row == 8 and col == 9):# if the solving function gets to this point, the board is solved and function is aborted, rerturns True
         return True
     if col == 9: # goes to the next row
         row += 1
         col = 0
-    if r[row][col] != "." or r[row][col] == "X": # skips the cell if it is already filled
-        return solve(row, col + 1)
+    if boardData[row][col] != "." or boardData[row][col] == "X": # skips the cell if it is already filled
+        return solve_puzzle(row, col + 1)
     for num in range(1, 10): # tries numbers 1-9
-        if check(row, col, num): # checks if the number is valid
-            r[row][col] = num
-            if solve(row, col + 1): # recursively calls the function
+        if check_if_valid(row, col, num): # checks if the number is valid
+            boardData[row][col] = num
+            if solve_puzzle(row, col + 1): # recursively calls the function
                 return True
-        r[row][col] = "." # if the number is not valid, it is reset
+        boardData[row][col] = "." # if the number is not valid, it is reset
         deltaTime = datetime.now() - startTime
         if deltaTime.total_seconds() > 5: # if the function takes longer than 5 seconds, it is aborted
             break
     return False 
 
-def solution():
+def check_solution():
     logic = True
     for i in range(9): 
-       if "." in r[i]:
+       if "." in boardData[i]:
             logic = False
     return logic
 
 #main loop
-r[pos[0]][pos[0]] = "X" # sets the starting position
-print(message,board())
+boardData[pos[0]][pos[0]] = "X" # sets the starting position
+print(message,create_board())
 def on_key_release(key): # listens for key presses
-    global s
+    global solved
     if key == Key.right: 
-        if s == False:
-            right()
+        if solved == False:
+            move_right()
     elif key == Key.left:
-        if s == False:
-            left()
+        if solved == False:
+            move_left()
     elif key == Key.up:
-        if s == False:
-            up()
+        if solved == False:
+            move_up()
     elif key == Key.down:
-        if s == False:
-            down()
+        if solved == False:
+            move_down()
     elif key == Key.esc:
         print("Exiting...")
         sleep(1)
@@ -145,23 +144,21 @@ def on_key_release(key): # listens for key presses
         print("Solving...")
         for i in range(9): # removes the cursor from the board
             for j in range(9):
-                if r[i][j] == "X":
-                    r[i][j] = "."
-        solve(0,0)
-        if solution() == True:
-            s = True
-            os.system('cls')
-            print("Solved!",board())
+                if boardData[i][j] == "X":
+                    boardData[i][j] = "."
+        solved = True
+        solve_puzzle(0,0)
+        os.system('cls')
+        if check_solution() == True:
+            print("Solved!",create_board())
         else:
-            s = True
-            os.system('cls')
-            print("No solution found.",board())
+            print("No solution found.",create_board())
     else: # checks if the input is a number and if it is, it is entered into the board
         try: # using try except to catch invalid inputs (if key is pressed that is not number/letter it would crash the program otherwise)
-            if key.char in n:
-                num = int(key.char)
-                r[pos[0]][pos[1]] = num
-                right()
+            num = int(key.char)
+            if num > 0:
+                boardData[pos[0]][pos[1]] = num
+                move_right()
         except:
             print("Invalid input.")
     
